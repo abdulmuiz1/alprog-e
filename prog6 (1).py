4463e6bebb8240b003c76ecaa0340abec830f25a
abdulmuiz1 <muizabdmz@gmail.com>
2024-11-10 09:57:51 -0800

    Add files via upload

---
+++ prog6 (1).py
@@ -0,0 +1,25 @@
+# variabel assigment value
+x = 7
+y = 2
+print("nilai nilai dari x adalah : ", y)
+
+# aturan penamaan variabel
+luas = 65 #
+abdul_M = 90 #
+AbdulM = 7 #
+abdulm = 4 #
+Dul27 = 9 #
+print("nilai dari abdulm adalah : ", Dul27)
+
+
+a = 4 # integer bilangan bulat
+print("tipe data dari a : ", type(a))
+
+b = 4,7 # float bilangan pecahan
+print("tipe data dari b : ", type(b))
+
+c = "nevermind" # string, gabungan karakter
+print("tipe data dari c : ", type(c))
+
+d = False # Boolean, true false
+print("tipe data dari d : ", type(d))
