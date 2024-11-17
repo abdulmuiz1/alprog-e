e3438896b7ed44086a1ae15ab7535de7dc2e085a
abdulmuiz1 <muizabdmz@gmail.com>
2024-11-17 12:38:51 +0800

    Update prog5.py

--- prog5.py
+++ prog5.py
@@ -7,4 +7,4 @@ print("panjang = nim akhir")
 print("lebar = nim awal")
 
 print("luas=panjang * lebar")
-print("mencetak f (luas dari{panjang}X{lebar}adalah = {luas})")
+print("mencetak (f: luas dari{panjang}X{lebar}adalah = {luas)")
